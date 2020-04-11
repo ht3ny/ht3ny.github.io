f47441b953766e6d1d7bba7193711ef7ae0178d9
hjdteny <hjd.teny@gmail.com>
2020-04-11 11:48:24 +1000

    edit tooltip to add tooltiptext dynamically

--- assets/js/tooltips.js
+++ assets/js/tooltips.js
@@ -1,14 +1,12 @@
 $(document).ready(function (){
   var url = "/tooltips.json";
   $.get(url, function( data ) {
-    $('.tooltiptext').each(function (i, el) {
+    $('.tooltip').each(function (i, el) {
       var $el = $(el);
-      var key = $el.attr('id');
+      var key = $el.attr('data-key');
       var tooltip = data.entries[key]; // use id lookup for tooltip entries 
       if (tooltip)
-        $el.text(tooltip).wrapInner("<strong />"); // put text in the tooltiptext el
-      else 
-        $el.removeClass('tooltiptext'); // remove empty tooltip
+        $el.append('<span class="tooltiptext">' + tooltip +'</span>');
     });
   });
 });
