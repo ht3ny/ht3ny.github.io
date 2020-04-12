0c390b62b75a93e3e5d8abed6cfd1a47633a0e65
hjdteny <hjd.teny@gmail.com>
2020-04-12 13:45:45 +1000

    store data.entries

--- assets/js/tooltips.js
+++ assets/js/tooltips.js
@@ -1,10 +1,13 @@
+var entries = {};
+
 $(document).ready(function (){
   var url = "/tooltips.json";
   $.get(url, function( data ) {
+    entries = Object.create(data.entries);
     $('.tooltip').each(function (i, el) {
       var $el = $(el);
       var key = $el.attr('data-key');
-      var tooltip = data.entries[key]; // use id lookup for tooltip entries 
+      var tooltip = entries[key]; // use id lookup for tooltip entries 
       if (tooltip)
         $el.append('<span class="tooltiptext">' + tooltip +'</span>');
     });
